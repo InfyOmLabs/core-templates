e301ad08bcb388ac3f100f59e830b9967c6fae7c
dtannen <dtannen@gmail.com>
2016-03-22 21:49:09 -0400

    Update errors.blade.php
    Fix error in 5.2

--- views/common/errors.blade.php
+++ views/common/errors.blade.php
@@ -1,7 +1,9 @@
-@if($errors->any())
-    <ul class="alert alert-danger" style="list-style-type: none">
-        @foreach($errors->all() as $error)
-            <li>{!! $error !!}</li>
-        @endforeach
-    </ul>
+@if(!empty($errors))
+    @if($errors->any())
+        <ul class="alert alert-danger" style="list-style-type: none">
+            @foreach($errors->all() as $error)
+                <li>{!! $error !!}</li>
+            @endforeach
+        </ul>
+    @endif
 @endif
